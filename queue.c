568948c991991737b8e14ecd63aaf6bec5b19440
Feilan Jiang <f-jiang@github.com>
2016-11-22 03:46:41 -0500

    Fix parameter names for queue

--- queue.c
+++ queue.c
@@ -5,27 +5,27 @@ queue *queue_init(size_t elem_size) {
     return list_init(elem_size);
 }
 
-void queue_del(queue **s) {
-    list_del(s);
+void queue_del(queue **q) {
+    list_del(q);
 }
 
-size_t queue_get_len(const queue * const s) {
-    return list_get_len(s);
+size_t queue_get_len(const queue * const q) {
+    return list_get_len(q);
 }
 
-void *queue_front(const queue * const s) {
-    return list_front(s);
+void *queue_front(const queue * const q) {
+    return list_front(q);
 }
 
-void *queue_back(const queue * const s) {
-    return list_back(s);
+void *queue_back(const queue * const q) {
+    return list_back(q);
 }
 
-void queue_push(queue * const s, void *val) {
-    list_push_front(s, val);
+void queue_push(queue * const q, void *val) {
+    list_push_front(q, val);
 }
 
-void queue_pop(queue * const s) {
-    list_pop_back(s);
+void queue_pop(queue * const q) {
+    list_pop_back(q);
 }
 
--- queue.h
+++ queue.h
@@ -6,14 +6,14 @@
 typedef list queue;
 
 queue *queue_init(size_t elem_size);
-void queue_del(queue **s);
+void queue_del(queue **q);
 
-size_t queue_get_len(const queue * const s);
+size_t queue_get_len(const queue * const q);
 
-void *queue_front(const queue * const s);
-void *queue_back(const queue * const s);
+void *queue_front(const queue * const q);
+void *queue_back(const queue * const q);
 
-void queue_push(queue * const s, void *val);
-void queue_pop(queue * const s);
+void queue_push(queue * const q, void *val);
+void queue_pop(queue * const q);
 
 #endif
