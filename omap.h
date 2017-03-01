67e1068713327b48a6999ec51ebd9fe59e82f38f
Feilan Jiang <feilanjiang@gmail.com>
2017-03-01 16:43:32 -0500

    omap param name correction

--- omap.h
+++ omap.h
@@ -11,7 +11,7 @@ typedef struct ordered_map_t omap;
 omap *omap_init(size_t key_size, size_t val_size, int (*comp)(void *a, void *b));
 void omap_del(omap **m);
 
-size_t omap_get_count(const omap * const s);
+size_t omap_get_count(const omap * const m);
 
 bool omap_insert(omap * const m, void *key, void *val);
 bool omap_remove(omap * const m, void *key);
